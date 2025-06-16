d8c2ec576556af01c17239cd38210cfa50cc3f5f
NgmThanh <nmthanh99@gmail.com>
2025-06-17 00:50:04 +0200

    added lenis smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,30 @@ const durationDefault = 1.2;
 * Fire all scripts on page load
 */
 function initScript() {
+  initSmoothScroll();
   initScrollHighlight();
   initTextRevealAnimation();
   zoomInMaskOnScroll();
+}
+
+/**
+* Smooth scroll with Lenis Scroll
+*/
+function initSmoothScroll() {
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis();
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on('scroll', ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
 }
 
 /**
