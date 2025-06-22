851c32f325b0c9288fcc6e6587331b287854028f
NgmThanh <nmthanh99@gmail.com>
2025-06-22 12:23:58 +0200

    addded contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,8 @@ function initScript() {
   initScrollHighlight();
   initStorytellingScroll();
   initHorizontalScroll();
-  initShapeMaskSection();
+  initPinScrollHighlight();
+  initRollingText();
 }
 
 /**
@@ -140,7 +141,6 @@ function initParallaxEffect() {
 */
 function initScrollHighlight() {
   const splitElements = document.querySelectorAll('[data-scroll-highlight]');
-  const splitElementsPinned = document.querySelectorAll('[data-scroll-highlight-pin]');
 
   document.fonts.ready.then(() => {
     splitElements.forEach((el) => {
@@ -151,25 +151,6 @@ function initScrollHighlight() {
           start: 'top 90%',
           end: 'bottom 50%',
           scrub: 1,
-        },
-        duration: 3,
-        opacity: 0.2,
-        stagger: 0.2,
-        ease: "none",
-      })
-    });
-  });
-
-  document.fonts.ready.then(() => {
-    splitElementsPinned.forEach((el) => {
-      const split = new SplitText(el, { type: "chars,words" });
-      gsap.from(split.chars, {
-        scrollTrigger: {
-          trigger: "#action",
-          start: 'top top',
-          end: '+=300%',
-          scrub: 1,
-          pin: true,
         },
         duration: 3,
         opacity: 0.2,
@@ -219,7 +200,7 @@ function initStorytellingScroll() {
       tl.fromTo('.circle-transition', {
         scale: 0,
       }, {
-        duration: 1,
+        duration: 2,
         scale: 50,
         ease: "none",
       });
@@ -228,59 +209,76 @@ function initStorytellingScroll() {
 }
 
 /**
- * Zoom in effect on scroll contact
+ * Text hightlight on scroll with ScrollTrigger (pinned)
  */
-function initShapeMaskSection() {
-  const mask = document.querySelector(".masked-img-container");
-  const img = document.querySelector(".masked-img");
-
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#shape-scroll",
-      start: "top top",
-      end: "+=400%",
-      scrub: true,
-      pin: true,
-      markers: true,
-    }
-  });
-
-  // Storytelling title reveal animation
-  const splitTitle = document.querySelectorAll('[data-title-reveal]');
+function initPinScrollHighlight() {
+  const splitElementsPinned = document.querySelectorAll('[data-scroll-highlight-pin]');
 
   document.fonts.ready.then(() => {
-    splitTitle.forEach((el) => {
-      const split = new SplitText(el, {
-        type: "chars, words, lines",
-        linesClass: "line-js",
-      });
-
-      tl.fromTo(split.chars, {
-        opacity: 0,
-        yPercent: 150,
-        markers: true,
-      }, {
-        scale: 1,
-        opacity: 1,
-        yPercent: 0,
-        duration: 2,
-        stagger: staggerTitleDefault,
-        ease: "power3.out",
-      }, 0);
+    splitElementsPinned.forEach((el) => {
+      const split = new SplitText(el, { type: "chars,words" });
+      gsap.from(split.chars, {
+        scrollTrigger: {
+          trigger: "#action",
+          start: 'top top',
+          end: '+=300%',
+          scrub: 1,
+          pin: true,
+        },
+        duration: 3,
+        opacity: 0.2,
+        stagger: 0.2,
+        ease: "none",
+      })
     });
   });
+}
 
-  tl.to(mask, {
-    duration: 3,
-    maskSize: "300%",
-    webkitMaskSize: "300%",
-    ease: "power4.in"
-  });
+/**
+* Rolling text
+*/
+function initRollingText() {
 
-  tl.to(img, {
-    scale: 1,
-    ease: "power2.out"
-  }, 0);
+  let direction = 1; // 1 = forward, -1 = backward scroll
+
+  const roll1 = roll(".rolling-text", { duration: 10 }),
+    roll2 = roll(".rollingText02", { duration: 10 }, true),
+    scroll = ScrollTrigger.create({
+      onUpdate(self) {
+        if (self.direction !== direction) {
+          direction *= -1;
+          gsap.to([roll1, roll2], { timeScale: direction, overwrite: true });
+        }
+      }
+    });
+
+  // helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
+  function roll(targets, vars, reverse) {
+    vars = vars || {};
+    vars.ease || (vars.ease = "none");
+    const tl = gsap.timeline({
+      repeat: -1,
+      onReverseComplete() {
+        this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+      }
+    }),
+      elements = gsap.utils.toArray(targets),
+      clones = elements.map(el => {
+        let clone = el.cloneNode(true);
+        el.parentNode.appendChild(clone);
+        return clone;
+      }),
+      positionClones = () => elements.forEach((el, i) => gsap.set(clones[i], { position: "absolute", overwrite: false, top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth) }));
+    positionClones();
+    elements.forEach((el, i) => tl.to([el, clones[i]], { xPercent: reverse ? 100 : -100, ...vars }, 0));
+    window.addEventListener("resize", () => {
+      let time = tl.totalTime(); // record the current time
+      tl.totalTime(0); // rewind and clear out the timeline
+      positionClones(); // reposition
+      tl.totalTime(time); // jump back to the proper time
+    });
+    return tl;
+  }
 }
 
 /**
